c9f7360c6a52ff5e35970be8d7a8cbc9f428f2a2
Yunchieh Wu <yunchiew@node7.maas>
2020-07-23 08:49:17 +0000

    runs everything together, still need to finish TODO

--- runTShark.sh
+++ runTShark.sh
@@ -1,10 +1,18 @@
 # TODO: include aboslute path
+
+# updating the text file of urls 
+python3 youtubeTrending.py
+
+# launch tshark in the background detecting quic traffic and storing it in packets.pcap
 sudo tshark -i eno4 -f "udp port 443 or udp port 80" -w /packets.pcap &
 
+# start obtaining screenshots
 node parallel.js
 
+# kill the first tshark 
 pidSudo=`ps -ef | grep "sudo tshark" | grep -v grep | awk '{print $2}'`
 sudo kill -9 $pidSudo
 
+# kill the second tshark 
 pidTshark=`ps -ef | grep tshark | grep -v grep | awk '{print $2}'`
 sudo kill -9 $pidTshark
