068f6a2e463e9fb12adaee415013dae31676b44f
Yunchieh Wu <yunchiew@node7.maas>
2020-07-25 01:30:24 +0000

    can etiher run quic or tcp depending on input parameter

--- runTShark.sh
+++ runTShark.sh
@@ -1,10 +1,20 @@
 # TODO: include aboslute path
 
+traffic="quic"
+
 # updating the text file of urls 
 python3 youtubeTrending.py
 
-# launch tshark in the background detecting quic traffic and storing it in packets.pcap
-sudo tshark -i eno4 -f "udp port 443 or udp port 80" -w /packets.pcap &
+if [ $1 == "tcp" ];
+then
+    echo "Capturing TCP"
+    # launch tshark in the background detecting tcp traffic and storing it in packetsTCP.pcap
+    sudo tshark -i eno4 -f "tcp" -w /packetsTCP.pcap &
+else
+    echo "Capturing QUIC"
+    # launch tshark in the background detecting quic traffic and storing it in packetsQuic.pcap
+    sudo tshark -i eno4 -f "udp port 443 or udp port 80" -w /packetsQuic.pcap &
+fi
 
 # start obtaining screenshots
 node parallel.js
